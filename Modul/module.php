31eff826b438f4dbfa5e39813b72c6d2aa0d5b40
DeMike <37411507+DeMikey@users.noreply.github.com>
2019-03-20 08:05:53 +0100

    Update module.php
    tipping fix

--- Modul/module.php
+++ Modul/module.php
@@ -788,7 +788,7 @@ class MoehlenhoffAlpha2 extends IPSModule
 		
 		foreach(self::$values as $key => $value) {
 			if(self::ReduceToIdent($key) == preg_replace('/\d+/', '%d', $Ident)) {
-				if(strpos($Key, "%d")) {
+				if(strpos($key, "%d")) {
 					preg_match_all('!\d+!', $Ident, $matches);
    					return str_replace('%d_', '"nr=' + implode(' ', $matches[0]) + '"/', $key);
 				}
